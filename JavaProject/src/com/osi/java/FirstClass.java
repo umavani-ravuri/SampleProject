ba79671a38b8f1e5bbea9f6a86e8db326b40daf7
umavani-ravuri <umavani22@gmail.com>
2017-04-28 15:56:09 +0530

    FEATURE/B-34561 git hub code changes

---
+++ JavaProject/src/com/osi/java/FirstClass.java
@@ -0,0 +1,12 @@
+package com.osi.java;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String sampleString="sampleOne";
+		System.out.println(sampleString);
+
+	}
+
+}
